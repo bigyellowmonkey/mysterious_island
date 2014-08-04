34a08c967272d72c20cab2b4c1dfe3bab6de9906
Justin Wong <jmnwong@gmail.com>
2014-08-04 14:06:41 -0400

    init

---
+++ hello.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+	name = "User1"
+	links = ['Listings', 'Leases',
+			'Projects', 'Associates',
+			'Company', 'Press', 'Blog', 'Contact']
+	return render_template('index.html', name=name, links=links)
+
+if __name__ == "__main__":
+	app.run()
