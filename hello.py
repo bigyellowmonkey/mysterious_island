3b6e4e2d5539698c8eca7ef3500174df6a6e981f
f2sun <Sunny2030@gmail.com>
2014-09-04 22:03:50 -0400

    hello.py updated.

--- hello.py
+++ hello.py
@@ -5,12 +5,15 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-	name = "User1"
-	links = ['Listings', 'Leases',
-			'Pre-Construction', 
-			'About Us', 'Contact']
-	return render_template('index.html', name=name, links=links)
+def index():
+	return render_template('index.html')
 
+@app.route('/Contact' , methods = ['GET', 'POST'])
+@app.route('/Contact.html' , methods = ['GET', 'POST'])
+def contacts():
+	
+	return render_template('Contact.html')
+
+	
 if __name__ == "__main__":
 	app.run()
