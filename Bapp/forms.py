4e43788f19a5ea989b35b02b2a3ddf77d9f3d545
Yong <yongxzhou@gmail.com>
2016-01-22 23:56:02 -0500

    test

---
+++ Bapp/forms.py
@@ -0,0 +1,8 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, BooleanField, TextAreaField
+from wtforms.validators import Required, Email
+
+class MailForm(Form):
+	nameid = StringField('Name', validators = [Required()])
+	email = StringField('Email', validators = [Email()])
+	message = TextAreaField('Message')
